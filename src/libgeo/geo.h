e1b406d69ee2d5038ddb6bd91c44b83509a7f950
Mihon <misha.2003ckyk@mail.ru>
2022-05-06 22:56:12 +0700

    adding functions

--- src/libgeo/geo.h
+++ src/libgeo/geo.h
@@ -3,7 +3,10 @@ typedef struct figure {
     char Name[6];
     char Name_full[15];
     double x, y, r;
-}figure;
+} figure;
 void pars_strok(char str[], int n, struct figure a[]);
 int error_code(char str[], int n, struct figure a[]);
 void intersection(int i, struct figure a[], int n);
+int input(char str[],struct figure a[]);
+double perimeter(struct figure a[], int i);
+double area(struct figure a[], int i);
